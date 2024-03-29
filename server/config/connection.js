b77c216b0abd080d1df2d990c9399a38f81cb3e3
Jakewilly14 <jakewilly14@gmail.com>
2024-03-29 18:37:04 -0500

    Heroku MongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/IMSDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   ssl: true, // Enable SSL
-  sslValidate: true, // Validate SSL certificates
 });
 
 module.exports = mongoose.connection;
